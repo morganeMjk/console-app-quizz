64b57403e109be14716591f363fdb6638b9625ea
MorganeMjk <morgane.mjk@outlook.fr>
2023-12-01 11:56:56 +0100

    chore: enhance error message display in Verify method

--- Response.cs
+++ Response.cs
@@ -6,32 +6,40 @@ public static class Response
 {
     public static bool Verify(Question currentQuestion, string userResponse)
     {
-        // Vérifier si la réponse de l'utilisateur est un int, et qu'elle est incluse parmis les choix possibles
-        if (!int.TryParse(userResponse, out int userResponseInt) || userResponseInt < 1 || userResponseInt > currentQuestion.Options.Count)
-        {
-            Console.WriteLine("Erreur : Veuillez saisir une réponse valide.");
-            return false;
-        }
-        else
-        {
-            Console.Clear();
 
-            // Vérifier si la réponse est correcte et incrémenter le score en cas de réponse correcte
-            if (userResponseInt == currentQuestion.CorrectOptionIndex)
+        while (true)
+        {
+            // Vérifier si la réponse de l'utilisateur est un int, et qu'elle est incluse parmi les choix possibles
+            if (!int.TryParse(userResponse, out int userResponseInt) || userResponseInt < 1 || userResponseInt > currentQuestion.Options.Count)
             {
-                Console.WriteLine("Bonne réponse :)");
-                Console.WriteLine("Appuyez sur une touche pour continuer");
-                Console.ReadLine();
-                Console.Clear();
-                return true;
+                Console.ForegroundColor = ConsoleColor.Red; // Set text color to red
+                Console.WriteLine("Erreur : Veuillez saisir une réponse valide.");
+                Console.ResetColor(); // Reset text color to default
+                // Prompt user for input again
+                Console.WriteLine("Réessayez :");
+                userResponse = Console.ReadLine();
             }
             else
             {
-                Console.WriteLine("Mauvaise réponse :(");
-                Console.WriteLine("Appuyez sur une touche pour continuer");
-                Console.ReadLine();
                 Console.Clear();
-                return false;
+
+                // Vérifier si la réponse est correcte et incrémenter le score en cas de réponse correcte
+                if (userResponseInt == currentQuestion.CorrectOptionIndex)
+                {
+                    Console.WriteLine("Bonne réponse :)");
+                    Console.WriteLine("Appuyez sur une touche pour continuer");
+                    Console.ReadLine();
+                    Console.Clear();
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Mauvaise réponse :(");
+                    Console.WriteLine("Appuyez sur une touche pour continuer");
+                    Console.ReadLine();
+                    Console.Clear();
+                    return false;
+                }
             }
         }
     }
